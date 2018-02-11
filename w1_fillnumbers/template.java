9dd9a89897af3e1ec5bd3408622ccf1d768a63d7
Pandarison <pandarison@gmail.com>
2018-02-11 13:15:20 +0000

    a+b

---
+++ w1_fillnumbers/template.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+{USERCODE}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution x = new Solution();
+        x.run();
+        
+    }
+}
